e42485e0bd9ee37ea8daa1e463f414f5278660c6
jussimar <jussimar@jussimarleal.com.br>
2025-06-27 22:43:37 -0300

    inserindo estilo e no flat para rolar a tela

--- views/Itanhaem.tsx
+++ views/Itanhaem.tsx
@@ -7,7 +7,7 @@ export default function Itanhaem(){
     const [dados, setDados] = useState<any[]>([]);
 
     async function buscaTempo(){
-        const response = await Api.get('weather?array_limit=3&fields=only_results,temp,city_name,forecast,condition,max,min,description,date&key=f5179305&city_name=Itanhaem,SP');
+        const response = await Api.get('weather?array_limit=4&fields=only_results,temp,city_name,forecast,condition,max,min,description,date&key=f5179305&city_name=Itanhaem,SP');
         setDados(response.data.forecast);
     }
     useEffect(
@@ -24,6 +24,7 @@ export default function Itanhaem(){
                 renderItem={({item})=> <Tempo date={item.date} 
                 max={item.max} min={item.min} description={item.description} 
                 condition={item.condition} />}
+                style={styles.lista}
             />
         </View>
     );
@@ -39,5 +40,9 @@ const styles = StyleSheet.create({
     Texto:{
         fontSize:20,
     },
+    lista:{
+        height:'70%',
+        width:'100%'
+    }
     
 });
